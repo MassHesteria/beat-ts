802d3991ccd0525fe4c39a2075a350aadd571760
MassHesteria <brad.hester@gmail.com>
2025-12-25 22:21:22 -0600

    Some args processing

--- beat.ts
+++ beat.ts
@@ -1,1 +1,9 @@
-console.log("beat v2")
+const args = Bun.argv.slice(2);
+
+if (args.includes("-create:bps")) {
+  console.log("Creating bps...");
+} else if (args.includes("-apply:bps")) {
+  console.log("UNIMPLEMENTED: Applying bps...");
+} else {
+  console.log("beat v2")
+};
