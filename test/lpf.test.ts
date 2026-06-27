6a18b1d3091fe5601dca867ae578be1d6f77ebc2
MassHesteria <brad.hester@gmail.com>
2026-06-27 11:04:07 -0500

    Turned off logs

--- test/lpf.test.ts
+++ test/lpf.test.ts
@@ -5,7 +5,7 @@ test("lpf", () => {
   const s = "banana";
   const arr = new Uint8Array(s.split("").map(x => x.charCodeAt(0)));
   const sa = suffix_array(arr, true);
-  console.log(sa.offsets)
-  console.log(sa.lengths)
+  //console.log(sa.offsets)
+  //console.log(sa.lengths)
   expect(sa.lengths).toEqual([0, 0, 0, 3, 2, 1, 0]);
 });
