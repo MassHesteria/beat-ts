3bfb12357cd36f1da757f96dc8cce5f9cf718953
MassHesteria <brad.hester@gmail.com>
2026-06-27 11:47:52 -0500

    Removed alt lpf solution

--- test/lpf.test.ts
+++ test/lpf.test.ts
@@ -9,14 +9,3 @@ test("lpf", () => {
   console.log(sa.lengths)
   expect(sa.lengths).toEqual([0, 0, 0, 3, 2, 1, 0]);
 });
-
-
-test("alt lpf", () => {
-  const s = "banana";
-  const arr = new Uint8Array(s.split("").map(x => x.charCodeAt(0)));
-  const sa = suffix_array(arr, true, true);
-  console.log(sa.offsets)
-  console.log(sa.lengths)
-  expect(sa.lengths).toEqual([0, 0, 0, 3, 2, 1, 0]);
-  //expect(sa.offsets).toEqual([0, 0, 0, 3, 2, 1, 0]);
-});
